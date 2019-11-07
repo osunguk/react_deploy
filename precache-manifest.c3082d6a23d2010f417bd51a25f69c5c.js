5c65e4681bc91e5b72f50eead81cbe80dca3e3ff
osunguk <osunguk@gmail.com>
2019-11-07 14:52:33 +0900

    Updates

---
+++ precache-manifest.c3082d6a23d2010f417bd51a25f69c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a341b1b367e33018ad422ed2ed2854bf",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "c17040bde538bfe0d6e6",
+    "url": "/react_deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ecd187af8bcf23c2ba8e",
+    "url": "/react_deploy/static/js/2.44a57036.chunk.js"
+  },
+  {
+    "revision": "c17040bde538bfe0d6e6",
+    "url": "/react_deploy/static/js/main.063297c8.chunk.js"
+  },
+  {
+    "revision": "7124b2dbe8af88e8ddad",
+    "url": "/react_deploy/static/js/runtime-main.a243e8fa.js"
+  }
+]);
