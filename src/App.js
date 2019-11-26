28a08d9da6e10b230ada3e76fc3501d82d65faed
osunguk <osunguk@gmail.com>
2019-11-26 11:25:51 +0900

    create Counter.js Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>배포 연습중</h1>
+      <Counter />
     </div>
   );
 }
